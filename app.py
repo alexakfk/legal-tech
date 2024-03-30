7b718b8587942f586cf26febcf70059e09bd3a5c
jackieBack <52051201+jackieBack@users.noreply.github.com>
2024-03-30 18:26:36 +0000

    Add login form

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ import numpy as np
 import pandas as pd
 from st_login_form import login_form
 
+import streamlit as st
+
+from st_login_form import login_form
+
 client = login_form()
 
-st.write(client)
+if st.session_state["authenticated"]:
+    if st.session_state["username"]:
+        st.success(f"Welcome {st.session_state['username']}")
+    else:
+        st.success("Welcome guest")
+else:
+    st.error("Not authenticated")
